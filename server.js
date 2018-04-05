bc9e4745a44cabeb85b084a0483ae7cd02219d01
Binh Trinh Dinh <binh.trinh@titancorp.vn>
2018-04-05 11:08:31 +0700

    update 2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const routes = require('./routers');
 const Api = require('./routers/api');
-// test
-var app = express();
 
+var app = express();
 app.get('/', routes);
 app.use('/api', Api);
 
