71ffc1bb1d75bcee44867d749703c0bcc1b39abf
dbthanhbinh <dbthanhbinh@gmail.com>
2018-04-22 13:18:27 +0700

    updated: Backend

--- frontend/src/components/layouts/Layout.jsx
+++ frontend/src/components/layouts/Layout.jsx
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+// import { Link } from 'react-router-dom';
+import Sidebar from '../containers/sidebar/sidebar';
+import Footer from '../containers/footer/footer';
 class Layout extends Component {
     render() {
     return (
         <div>
-            <div>
-                <h1>Header</h1>
-            </div>
-            <div>
-                <ul>
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/about'}>About</Link></li>
-                </ul>
-            </div>
-            <div>
-                { this.props.children }
-            </div>
-            <div>
-                <h1>Footer</h1>
-            </div>
+            <Sidebar />                       
+            { this.props.children }
+            <Footer />
         </div>
         );
     }
