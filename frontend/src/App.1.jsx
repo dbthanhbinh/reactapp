915baae70fff9cc4e83e12de1cd166d276ffacf4
Binh Trinh Dinh <binh.trinh@titancorp.vn>
2018-05-11 13:39:21 +0700

    intergrare semantic react

---
+++ frontend/src/App.1.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/containers/home/index';
+import Layout from './components/layouts/Layout';
+import Service from './core/services/index';
+ 
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+            {/* <Layout>
+                <Route exact path="/" component={Home} />                
+            </Layout> */}
+
+            {/* <Route exact path="/" component={Home} /> */}
+        </BrowserRouter>
+       );
+  }
+}
+
+export default App;
