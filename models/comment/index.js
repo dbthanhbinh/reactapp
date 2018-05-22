4fc558615eff47c407b5250a8a7c0716caba3d52
dbthanhbinh <dbthanhbinh@gmail.com>
2018-05-22 13:08:29 +0700

    updated

--- models/comment/index.js
+++ models/comment/index.js
@@ -1,14 +1,9 @@
 const mongoose = require('../../connecting');
 const Schema = mongoose.Schema;
-<<<<<<< HEAD
 const baseModel = require('../base');
 const commentSchema = require('./schemas/Comment');
 var schemaValidation = require('../../providers/schemaValid');
 schemaValidation = new schemaValidation(commentSchema)
-=======
-var schemaValidation = require('../../providers/schemaValid');
-const baseModel = require('../base');
->>>>>>> f00d754c7c5fe48ce98c99ef48c6a42e345b95ce
 
 const commentSchema = require('./schemas/Comment');
 var CommentSchema = new Schema(commentSchema, { collection: 'comments' });
